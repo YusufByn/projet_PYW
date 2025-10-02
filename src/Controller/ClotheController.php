a9ef7489c7d79bdfe729f8f128201c75d63ce54e
WilliamPeynichou <williampro1711@gmail.com>
2025-10-02 10:03:57 +0200

    CRUD Image

---
+++ src/Controller/ClotheController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Clothe;
+use App\Form\ClotheType;
+use App\Repository\ClotheRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/clothe')]
+final class ClotheController extends AbstractController
+{
+    #[Route(name: 'app_clothe_index', methods: ['GET'])]
+    public function index(ClotheRepository $clotheRepository): Response
+    {
+        return $this->render('clothe/index.html.twig', [
+            'clothes' => $clotheRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_clothe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $clothe = new Clothe();
+        
+        // Assigner automatiquement l'utilisateur connecté comme propriétaire
+        $clothe->setUser($this->getUser());
+        $clothe->setCurrentBorrower(null); // Pas d'emprunteur au début
+        
+        $form = $this->createForm(ClotheType::class, $clothe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = time().'-'.$originalFilename.'.'.$imageFile->guessExtension();
+                
+                // Définir le dossier d'upload
+                $uploadDir = $this->getParameter('kernel.project_dir').'/public/images';
+                
+                // Déplacer le fichier
+                $imageFile->move($uploadDir, $newFilename);
+                $clothe->setImg($newFilename);
+            }
+
+            $entityManager->persist($clothe);
+            $entityManager->flush();
+        }
+
+        return $this->render('clothe/new.html.twig', [
+            'clothe' => $clothe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_clothe_show', methods: ['GET'])]
+    public function show(Clothe $clothe): Response
+    {
+        return $this->render('clothe/show.html.twig', [
+            'clothe' => $clothe,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_clothe_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Clothe $clothe, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ClotheType::class, $clothe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_clothe_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('clothe/edit.html.twig', [
+            'clothe' => $clothe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_clothe_delete', methods: ['POST'])]
+    public function delete(Request $request, Clothe $clothe, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$clothe->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($clothe);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_clothe_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ClotheType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Clothe;
+use App\Entity\State;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClotheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false,
+                'required' => false,
+            ])
+            ->add('img', HiddenType::class)
+            ->add('user', HiddenType::class)
+            ->add('currentBorrower', HiddenType::class)
+            ->add('state', EntityType::class, [
+                'class' => State::class,
+                'choice_label' => 'name',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Clothe::class,
+        ]);
+    }
+}
